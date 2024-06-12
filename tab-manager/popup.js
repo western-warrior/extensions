343dc3cc5f78ed7bb52c86dc9e191ec8b9f093ca
adi <@boomerang>
2024-06-12 14:46:12 -0500

    created basic design

--- tab-manager/popup.js
+++ tab-manager/popup.js
@@ -1,16 +1,27 @@
-var tasks = fetch(chrome.runtime.getURL("tasks.txt")).then((response) => response.text()).catch(error => console.error);
-tasks = await tasks;
-tasks = tasks.split("\n");
-var dates = [];
-var titles = [];
-for (const item of tasks) {
-    if (tasks.indexOf(item) % 2 == 0) {
-        titles.push(item);
-    } else {
-        dates.push(Number(item));
-    }
-}
-const template = document.getElementById("li_template");
+//var tasks = fetch(chrome.runtime.getURL("tasks.txt")).then((response) => response.text()).catch(error => console.error);
+//tasks = await tasks;
+//tasks = tasks.split("\n");
+//var dates = [];
+//var titles = [];
+//for (const item of tasks) {
+//    if (tasks.indexOf(item) % 2 == 0) {
+//        titles.push(item);
+//    } else {
+//        dates.push(Number(item));
+//    }
+//}
+if (true) {
+    window.localStorage.setItem('dates', '31 22 2 111');
+    window.localStorage.setItem('titles', 'Garden Vacuum Housekeeping Kitchen');
+    window.localStorage.setItem('run', true);}
+
+var dates = window.localStorage.getItem('dates');
+console.log(dates);
+dates = dates.split(" ");
+console.log(dates);
+var titles = window.localStorage.getItem('titles');
+titles = titles.split(" ");
+
 for (const item of dates) {
     console.log(titles[dates.indexOf(item)]);
     var title = "Check on " + titles[dates.indexOf(item)] + " in:";
@@ -32,5 +43,7 @@ for (const item of dates) {
     document.getElementById("list").appendChild(newDiv);
 }
 
-function resetDate() {
+function resetDate(value) {
+    const set = chrome.storage.local.set({'dates': defaultDates}, function() {return});
+
 }
